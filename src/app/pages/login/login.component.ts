d086e40266e69d8ffba5645cebd47d78dcb5b9fc
Matias Leonel Ramirez <matiasleonel.ar>
2024-10-29 11:45:19 -0300

    Desarrollo del Formulario Login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AccesoService } from '../../services/acceso.service';
+import { Router } from '@angular/router';
+//importo lo que voy a usar en la logica
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Login } from '../../interfaces/Login';
+//Angular Material
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule,MatFormFieldModule,MatInputModule,MatButtonModule,ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  //
+  private accesoService = inject(AccesoService);
+  private router = inject(Router);
+  public formBuil = inject(FormBuilder);
+  
+  //Formulario para Iniciar
+  public formLogin: FormGroup = this.formBuil.group({
+    correo:['',Validators.required],
+    clave:['',Validators.required]
+  })
+
+  //Metodo para Iniciar Sesion
+  iniciarSesion(){
+    if(this.formLogin.invalid)return;
+    const objeto:Login = {
+      correo: this.formLogin.value.correo,
+      clave: this.formLogin.value.clave
+    }
+
+    //Validacion por "isSucces" de la Api
+    this.accesoService.login(objeto).subscribe({
+      next:(data) =>{
+        if(data.isSuccess){
+          localStorage.setItem("token",data.token)
+          this.router.navigate(['inicio']) //"inicio" debe ser igual a lo mapeado en approutes.ts
+        }else{
+          alert("Las Credenciales son Incorrectas.")
+        }
+      },
+      //Esto por si tira error
+      error:(error) =>{
+        console.log(error.message);
+      }
+    })
+  }
+
+  //Metodo para Registro
+  registrarse(){
+    this.router.navigate(['registro']) //Redirige a registro
+  }
 }
--- src/app/services/acceso.service.ts
+++ src/app/services/acceso.service.ts
@@ -3,6 +3,8 @@ import { inject, Injectable } from '@angular/core';
 import { appsettings } from '../settings/appsettings';
 import { Observable } from 'rxjs';
 import { ResponseAcceso } from '../interfaces/ResponseAcceso';
+import { Usuario } from '../interfaces/Usuario';
+import { Login } from '../interfaces/Login';
 
 @Injectable({
   providedIn: 'root'
@@ -14,7 +16,12 @@ export class AccesoService {
 
   constructor() { }
 
-  registrarse(objeto:Usuario) : Observable<ResponseAcceso>{
+// Conexion Registro y Login
+  registrarse(objeto:Usuario):Observable<ResponseAcceso>{
     return this.http.post<ResponseAcceso>(`${this.baseUrl}Acceso/Registrarse`, objeto)
+  }
+
+  login(objeto:Login):Observable<ResponseAcceso>{
+    return this.http.post<ResponseAcceso>(`${this.baseUrl}Acceso/Login`, objeto)
   }
 }
