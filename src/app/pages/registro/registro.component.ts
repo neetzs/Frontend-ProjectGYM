275db8b06a5b7d131dc47bec3117253d9f51bf38
Matias Leonel Ramirez <matiasleonel.ar>
2024-10-30 16:25:34 -0300

    Desarrollo de Formulario de Registro - PROBADO

--- src/app/pages/registro/registro.component.ts
+++ src/app/pages/registro/registro.component.ts
@@ -1,12 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AccesoService } from '../../services/acceso.service';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Usuario } from '../../interfaces/Usuario';
+//Angular Material
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+
+
 
 @Component({
   selector: 'app-registro',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule,MatFormFieldModule,MatInputModule,MatButtonModule,ReactiveFormsModule],
   templateUrl: './registro.component.html',
   styleUrl: './registro.component.css'
 })
 export class RegistroComponent {
+  private accesoService = inject(AccesoService);
+  private router = inject(Router);
+  public formBuil = inject(FormBuilder);
+  
+  //Formulario para Iniciar
+  public formRegistro: FormGroup = this.formBuil.group({
+    nombre:['',Validators.required],
+    correo:['',Validators.required],
+    clave:['',Validators.required]
+  });
 
+  //Metodo para Registrarse
+  registrarse(){
+    if(this.formRegistro.invalid) return;
+
+    const objeto:Usuario = {
+      nombre: this.formRegistro.value.nombre,
+      correo: this.formRegistro.value.correo,
+      clave: this.formRegistro.value.clave
+    }
+
+      this.accesoService.registrarse(objeto).subscribe({
+        next: (data) =>{
+          if(data.isSuccess){
+            this.router.navigate([''])
+          }else{
+            alert("No se pudo Registrar")
+          }
+        }, error:(error)=>{
+          console.log(error.message);
+        }
+    })
+  }
+
+  volver(){
+    this.router.navigate([''])
+  }
 }
